1aab597b3cfc328c28cd938bddd05f401b193aa2
Victor Wads <victor.laureano@gmail.com>
2024-01-08 18:32:36 -0300

    add google sign in

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -7,12 +7,16 @@
 
 import FirebaseCore
 import SwiftUI
+import GoogleSignIn
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
 }
--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -8,25 +8,28 @@
 import _AuthenticationServices_SwiftUI
 import FirebaseAuth
 import SwiftUI
+import GoogleSignInSwift
 
 
 struct ContentView: View {
     
     @ObservedObject var authManager = AuthenticationManager()
+    var previewInfo: String? = nil
 
     var body: some View {
         VStack {
             Spacer()
-            if let userInfo = authManager.userInfo {
+            if let userInfo = previewInfo ?? authManager.userInfo {
                 Text("Olá, \(userInfo)")
                     .foregroundColor(.white)
                 Button("Logout") {
                     authManager.logOut()
                 }
-               .foregroundColor(.white)
-               .padding()
-               .background(Color.gray)
-               .cornerRadius(5)
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(.white)
+                .cornerRadius(5)
             } else {
                 SignInWithAppleButton(.signIn) { request in
                     request.nonce = authManager.getShaNonce()
@@ -43,12 +46,25 @@ struct ContentView: View {
                 }
                 .signInWithAppleButtonStyle(.whiteOutline)
                 .frame(height: 45)
-                .padding(25)
+                Button("Sign In With Google") {
+                    authManager.googleLogin()
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(.white)
+                .cornerRadius(5)
             }
             Spacer()
-        }.background(Color(hex: 0x282c34))
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(Color(hex: 0x282c34))
     }
+}
 
+#Preview {
+    ContentView(previewInfo: "User - email")
 }
 
 #Preview {
