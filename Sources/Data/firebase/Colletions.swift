4a301ca36c1e6b58dc3534c816ef096cf8f20eba
Victor Wads <victor.laureano@gmail.com>
2024-01-18 21:59:28 -0300

    Init Categories Files

--- Sources/Data/firebase/Colletions.swift
+++ Sources/Data/firebase/Colletions.swift
@@ -4,6 +4,7 @@ import Foundation
 struct Collections {
     static let Banks = "Banks"
     static let Users = "Users"
+    static let Categories = "Categories"
     static let Accounts = "Accounts"
     static let CreditCards = "CreditCards"
     static let Registers = "Registers"
---
+++ Sources/Data/models/Category.swift
@@ -0,0 +1,25 @@
+//
+//  Category.swift
+//  GoldenUnicorn
+//
+//  Created by Victor Wads Laureano on 17/01/24.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+struct Category: Codable, Identifiable {
+    var parentId: String? = nil
+    @DocumentID var id: String? = nil
+    var name: String = ""
+    var icon: String? = nil
+    var color: String? = nil
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case icon
+        case parentId
+        case color
+    }
+}
