d0c06c6bb65bef281cf7922b3b7afedff7631dc5
Victor Wads <victor.laureano@gmail.com>
2025-05-07 23:56:27 -0300

    timeline category filter

--- src/features/tabs/timeline/TimelineScreen.tsx
+++ src/features/tabs/timeline/TimelineScreen.tsx
@@ -1,5 +1,5 @@
 import "./TimelineScreen.css";
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useSearchParams } from 'react-router-dom';
 import { useEffect, useState } from "react";
 
 import Account from "../../../data/models/Account";
@@ -17,6 +17,8 @@ interface WithInfoRegistry extends AccountsRegistry {
   category?: Category;
   account: Account;
 }
+
+const categoryParamName = 'c';
 
 const TimelineScreen = () => {
   const [showArchived, setShowArchived] = useState(false)
@@ -42,7 +44,7 @@ const TimelineScreen = () => {
         setSelectedAccount(null);
       }
 
-      const items = registries.getAccountItems(params.id, showAll);
+      let items = registries.getAccountItems(params.id, showAll);
       setTotal(items.balance);
       setRegistries(items.registries.map((registry) => {
         return {
@@ -54,6 +56,25 @@ const TimelineScreen = () => {
     })();
   }, [params.id, showArchived]);
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const categoriaParam = searchParams.get(categoryParamName);
+  const categoriaIds = categoriaParam?.split(',') ?? [];
+  const hasCategoryFilter = categoriaIds.length > 0;
+  let filteredRegistries = registries;
+  if (hasCategoryFilter) {
+    filteredRegistries = registries.filter(r => r.categoryId && categoriaIds.includes(r.categoryId));
+  }
+
+  function addCategoryFilter(categoryId: string) {
+    const newParams = new URLSearchParams(searchParams);
+    const categoriaIds = newParams.get(categoryParamName)?.split(',') ?? [];
+    if (!categoriaIds.includes(categoryId)) {
+      categoriaIds.push(categoryId);
+    }
+    newParams.set(categoryParamName, categoriaIds.join(','));
+    setSearchParams(newParams);
+  }
+
   let perDayTotal = total;
   let currentDay = registries[0]?.date.getDate();
   return (
@@ -62,16 +83,16 @@ const TimelineScreen = () => {
         <div className="ScreenHeaderRow">
           <h1 className="ScreenTitle">Timeline</h1>
           <span className="RegistryCount">({registries.length}) Registros</span>
-          {selectedAccount && (
+          {(selectedAccount || hasCategoryFilter) && (
             <div className="SelectedBank">
-              <span>{selectedAccount.name}</span>
+              {selectedAccount && <span>{selectedAccount.name}</span>}
               <Link to={'/main/timeline'} className="ClearFilter">Mostrar todos</Link>
             </div>
           )}
         </div>
         <div className="ScreenHeaderRow">
           <div className="ScreenTotal">
-            <span>Total:</span>
+            <span>Balance:</span>
             <span className={`TotalValue ${total >= 0 ? "positive" : "negative"}`}>
               {formatNumber(total)}
             </span>
@@ -89,7 +110,7 @@ const TimelineScreen = () => {
         </div>
       </div>
       <div className="TimelineList">
-        {registries.map((registry) => {
+        {filteredRegistries.map((registry) => {
           const isCurrentDay = registry.date.getDate() === currentDay;
           if (!isCurrentDay) {
             currentDay = registry.date.getDate();
@@ -103,6 +124,7 @@ const TimelineScreen = () => {
             <div key={registry.id} className="TimelineItem" style={{opacity: registry.paid ? 1 : 0.7}}>
               {/* Área Esquerda: Círculo com cor da categoria */}
               <div
+                onClick={() => addCategoryFilter(registry.categoryId!)}
                 className="TimelineCategory"
                 style={{ backgroundColor: registry.category?.color ?? "#ccc" }}
               ></div>
@@ -112,7 +134,9 @@ const TimelineScreen = () => {
                 <div className="TimelineDescription">{registry.description}</div>
                 <div className="TimelineDetails">
                   <span className="TimelineDate">{registry.date.toLocaleDateString()}</span>
-                  {registry.categoryId && <span className="TimelineCategoryName">{registry.category?.name}</span>}
+                  {registry.categoryId && <div className="TimelineCategoryName" onClick={() => addCategoryFilter(registry.categoryId!)}>
+                    {registry.category?.name}
+                  </div>}
                   <Link to={'/main/timeline/' + registry.accountId}>
                     <span className="TimelineBankName">{registry.account.name}</span>
                   </Link>
