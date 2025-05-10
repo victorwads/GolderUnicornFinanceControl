afa2a58bc0329aa33ffe7e39f3b002c7f94e5b7b
Victor Wads <victor.laureano@gmail.com>
2025-05-10 04:05:57 -0300

    fix icons names with more than 1 dash

--- src/components/Icons.ts
+++ src/components/Icons.ts
@@ -2,24 +2,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import * as brands from '@fortawesome/free-brands-svg-icons'
 import * as solid from '@fortawesome/free-solid-svg-icons'
+import * as regular from '@fortawesome/free-regular-svg-icons'
 
-const Icons = { ...solid, ...brands }
+const Icons = { ...solid, ...brands, ...regular }
 const Icon = FontAwesomeIcon as (typeof FontAwesomeIcon & {all: typeof Icons});
 Icon.all = Icons;
 export default Icon;
 
 export type { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
-type IconList = typeof solid | typeof brands;
+type IconList = typeof solid | typeof brands | typeof regular;
 export type IconKey = keyof IconList;
 
 export function getIconByCaseInsensitiveName(name: string | IconDefinition): IconDefinition {
   if(typeof name !== 'string') return name;
-  const normalizedName = ICON_NAME_PREFIX + name.trim().toLowerCase().replace('-', '');
+  const normalizedName = ICON_NAME_PREFIX + name.trim().toLowerCase().replaceAll('-', '');
   const foundIcon = iconNames.find(iconName =>
     iconName.lowerCaseName === normalizedName
-  )?.icon;
-  
+  )?.icon;  
   return foundIcon ? foundIcon : Icons.faQuestion;
 }
 
@@ -30,7 +30,8 @@ interface IconSearchName {
 
 const iconNames: IconSearchName[] = [
   ...mapIconPack(solid),
-  ...mapIconPack(brands)
+  ...mapIconPack(brands),
+  ...mapIconPack(regular)
 ];
 const ICON_NAME_PREFIX = "fa";
 
