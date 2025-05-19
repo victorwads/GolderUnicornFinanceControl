0073d1027980c3a5babef5451f3b04788d53c3a8
Victor Wads <victor.laureano@gmail.com>
2025-05-19 02:11:41 -0300

    type select field

--- src/components/fields/SelectField.tsx
+++ src/components/fields/SelectField.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import BaseField from "./BaseField";
 
-interface SelectFieldProps {
+interface SelectFieldProps<T> {
   label: string;
-  value: string;
-  onChange: (value: string) => void;
-  options: { value: string; label: string }[];
+  value?: T;
+  onChange: (value: T) => void;
+  options: { value: T; label: string }[];
 }
 
-const SelectField: React.FC<SelectFieldProps> = ({
+const SelectField = <T extends string | number>({
   label,
   value,
   onChange,
   options,
-}) => {
+}: SelectFieldProps<T>) => {
   return (
     <BaseField label={label}>
       <select
         value={value}
-        onChange={(event) => onChange(event.target.value)}
+        onChange={(event) => onChange(event.target.value as T)}
       >
         {options.map((option) => (
           <option key={option.value} value={option.value}>
